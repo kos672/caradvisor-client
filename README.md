d52a8ea57f75fb7a39318cb73e8ac7a0adde2e68
Konstantin <kocc95@mail.ru>
2018-09-30 13:06:44 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Inz
+# caradvisor-cleint
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,6 +23,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to inz!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to caradvisor-cleint!');
   }));
 });
