baf8bd0ed9afeafdb5d2aa1a1913388b74ff420f
Konstantin <kocc95@mail.ru>
2018-10-07 00:05:26 +0200

    content component is created

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Question} from './question/question.model';
+import {Answer} from './answer/answer.model';
 
 @Component({
   selector: 'app-content',
@@ -6,6 +8,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+
+  messages: Question | Answer[];
 
   constructor() {
   }
