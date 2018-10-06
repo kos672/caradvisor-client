48af0e68e07dde6f72faef42927091b1635f6b89
Konstantin <kocc95@mail.ru>
2018-10-06 19:56:40 +0200

    answer component is created

---
+++ src/app/content/answer/answer.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Answer} from './answer.model';
+
+@Component({
+  selector: 'app-answer',
+  templateUrl: './answer.component.html',
+  styleUrls: ['./answer.component.css']
+})
+export class AnswerComponent implements OnInit {
+
+  @Input() answer: Answer;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
