df072a1e2be641d841baef9537d118348aa0952a
Konstantin <kocc95@mail.ru>
2018-10-06 19:57:36 +0200

    added Answer and Question model classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {ContentComponent} from './content/content.component';
 import {FooterComponent} from './footer/footer.component';
+import {AnswerComponent} from './content/answer/answer.component';
+import {QuestionComponent} from './content/question/question.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    AnswerComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule
