7b8ccb70b27b0f0c88fc45ed7992e190dbd20c3e
Konstantin <kocc95@mail.ru>
2018-10-14 16:54:08 +0200

    global constants class is added

---
+++ src/app/shared/global-constants.ts
@@ -0,0 +1,16 @@
+export class GlobalConstants {
+  /* Cookies */
+  static readonly IS_NAME_KNOWN = 'IS_NAME_KNOWN';
+  static readonly IS_ENGINE_TYPE_KNOWN = 'IS_ENGINE_TYPE_KNOWN';
+  static readonly IS_ENGINE_DISPLACEMENT_KNOWN = 'IS_ENGINE_DISPLACEMENT_KNOWN';
+  static readonly IS_TRANSMISSION_KNOWN = 'IS_TRANSMISSION_KNOWN';
+  static readonly IS_DRIVETRAIN_KNOWN = 'IS_drivetrain_KNOWN';
+  static readonly IS_DOORS_KNOWN = 'IS_DOORS_KNOWN';
+  static readonly IS_SEATS_KNOWN = 'IS_SEATS_KNOWN';
+
+  /* Boolean representation */
+  static readonly TRUE = 'true';
+  static readonly FALSE = 'false';
+
+
+}
