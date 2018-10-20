260b99e0298c53892ac337bbaaa8fff02c5cc6ca
Konstantin <kocc95@mail.ru>
2018-10-20 20:46:25 +0200

    footer component implementation

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MessageService} from '../shared/message.service';
+import {CookieService} from 'ngx-cookie-service';
+import {GlobalConstants} from '../shared/global-constants';
 
 @Component({
   selector: 'app-footer',
@@ -7,10 +10,25 @@ import {Component, OnInit} from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() {
+  @ViewChild('message') messageToSend;
+
+  constructor(private messageService: MessageService, private cookieService: CookieService) {
   }
 
   ngOnInit() {
+    this.cookieService.set(GlobalConstants.IS_NAME_KNOWN, GlobalConstants.FALSE);
+    this.cookieService.set(GlobalConstants.IS_ENGINE_TYPE_KNOWN, GlobalConstants.FALSE);
+    this.cookieService.set(GlobalConstants.IS_ENGINE_DISPLACEMENT_KNOWN, GlobalConstants.FALSE);
+    this.cookieService.set(GlobalConstants.IS_TRANSMISSION_KNOWN, GlobalConstants.FALSE);
+    this.cookieService.set(GlobalConstants.IS_DRIVETRAIN_KNOWN, GlobalConstants.FALSE);
+    this.cookieService.set(GlobalConstants.IS_DOORS_KNOWN, GlobalConstants.FALSE);
+    this.cookieService.set(GlobalConstants.IS_SEATS_KNOWN, GlobalConstants.FALSE);
+  }
+
+  send() {
+    let message = this.messageToSend.nativeElement.value;
+    this.messageService.onSend(this.messageToSend);
+    this.messageService.messages.push({text: message});
   }
 
 }
