b0c3e0fe50643745ff860dcdb6927412257f5dd3
Konstantin <kocc95@mail.ru>
2018-11-18 22:17:20 +0100

    Added CarDto model

---
+++ src/app/shared/cardto.model.ts
@@ -0,0 +1,11 @@
+export interface CarDto {
+  id: number;
+  manufacturer: string;
+  model: string;
+  engine: string;
+  engineDisplacement: number;
+  transmission: string;
+  drivetrain: string;
+  doors: number;
+  seats: number;
+}
