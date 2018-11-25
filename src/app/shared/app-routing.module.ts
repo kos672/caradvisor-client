b4f9ee0a66b0b2258bd69f05f7ceeb002dd5c3ac
Konstantin <kocc95@mail.ru>
2018-11-25 21:39:09 +0100

    added child path for Result page

--- src/app/shared/app-routing.module.ts
+++ src/app/shared/app-routing.module.ts
@@ -2,10 +2,15 @@ import {RouterModule} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {ProcessComponent} from '../process/process.component';
 import {ResultComponent} from '../result/result.component';
+import {CarDetailComponent} from '../result/car-detail/car-detail.component';
 
 const appRoutes = [
   {path: '', component: ProcessComponent, pathMatch: 'full'},
-  {path: 'result', component: ResultComponent}
+  {
+    path: 'result', component: ResultComponent, children: [
+      {path: ':id', component: CarDetailComponent}
+    ]
+  }
 ];
 
 @NgModule({
