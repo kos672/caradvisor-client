e74d74f073dbcd6842ee16038d0b8dd172ccba38
Konstantin <kocc95@mail.ru>
2019-01-20 20:48:26 +0100

    added login path and canActivate guards

--- src/app/shared/app-routing.module.ts
+++ src/app/shared/app-routing.module.ts
@@ -3,14 +3,17 @@ import {NgModule} from '@angular/core';
 import {ProcessComponent} from '../process/process.component';
 import {ResultComponent} from '../result/result.component';
 import {CarDetailComponent} from '../result/car-detail/car-detail.component';
+import {LoginComponent} from '../login/login.component';
+import {LoginActivate} from '../login/login-activate';
 
 const appRoutes = [
-  {path: '', component: ProcessComponent, pathMatch: 'full'},
+  {path: '', component: ProcessComponent, pathMatch: 'full', canActivate: [LoginActivate]},
   {
-    path: 'result', component: ResultComponent, children: [
+    path: 'result', component: ResultComponent, canActivate: [LoginActivate], children: [
       {path: ':id', component: CarDetailComponent}
     ]
-  }
+  },
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
